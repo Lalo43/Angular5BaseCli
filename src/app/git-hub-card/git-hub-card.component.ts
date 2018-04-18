7f89f38cbefcad15be66b03d19e5fd19ebaa3b13
JAVA8 <deophis@gmail.com>
2018-04-18 18:16:26 -0500

    actualizado

---
+++ src/app/git-hub-card/git-hub-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+
+@Component({
+    selector: 'app-git-hub-card',
+    templateUrl: './git-hub-card.component.html',
+    styleUrls: ['./git-hub-card.component.css']
+
+})
+export class GitHubCardComponent implements OnInit, OnDestroy {
+    constructor(){
+
+    }
+    ngOnInit(){        
+    }
+    ngOnDestroy(){        
+    }
+
+}
